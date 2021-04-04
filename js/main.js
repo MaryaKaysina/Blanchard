2fb55e1e6d77245d5d975d9b78c4a25ee465ae06
MaryaKaysina <61281550+MaryaKaysina@users.noreply.github.com>
2021-04-04 19:49:23 +0500

    add files

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+  // slider
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 9000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  });
+
+  //burger menu
+  $(".header__burger").click(function() {
+    if (!$(".burger__nav").hasClass("burger__nav--open")) {
+      $(".burger__nav").addClass("burger__nav--open");
+      $(".burger__menu").addClass("burger__menu--close");
+    } else {
+      $(".burger__nav").removeClass("burger__nav--open");
+      $(".burger__menu").removeClass("burger__menu--close");
+    }
+  });
+
+  //search
+  if ($(window).width() < 1250) {
+    $(".search__icon").click(function() {
+      if (!$(".header__search").hasClass("header__search--open")) {
+        $(".header__search").addClass("header__search--open");
+        $(".search__open").addClass("search__open--active");
+        $(".search__close").removeClass("search__close--hidden");
+      } else {
+        $(".header__search").removeClass("header__search--open");
+        $(".search__open").removeClass("search__open--active");
+      }
+    });
+    $(".search__close").click(function() {
+      $(".search__close").addClass("search__close--hidden");
+      $(".header__search").removeClass("header__search--open");
+      $(".search__open").removeClass("search__open--active");
+    });
+  }
+});
